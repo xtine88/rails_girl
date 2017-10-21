2160544fe684886ba2daa0d6c82f73c55dbe114f
Christine de la Fuente <c.delafuente88@gmail.com>
2017-10-22 01:00:29 +0200

    remove before_action to view in heroku

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,5 +1,5 @@
 class AdventuresController < ApplicationController
-  before_action :set_adventure, only: [:show, :edit, :update, :destroy]
+  # before_action :set_adventure, only: [:show, :edit, :update, :destroy]
 
   # GET /adventures
   # GET /adventures.json
@@ -10,6 +10,7 @@ class AdventuresController < ApplicationController
   # GET /adventures/1
   # GET /adventures/1.json
   def show
+     @adventure = Adventure.find(params[:id])
   end
 
   # GET /adventures/new
@@ -63,9 +64,11 @@ class AdventuresController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_adventure
-      @adventure = Adventure.find(params[:id])
-    end
+    # def set_adventure
+    #   # @adventure = Adventure.find(params[:id])
+    # end
+
+    #testing before action
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def adventure_params
