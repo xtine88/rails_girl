d6c7e4af6c55f32ba2f652fbfd87db1fe36c49ba
Christine de la Fuente <c.delafuente88@gmail.com>
2017-10-22 15:31:00 +0200

    created pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
   resources :adventures
   root to: 'adventures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
